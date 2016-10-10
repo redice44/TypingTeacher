f608714c2410df8f2e9a86ed90aeed08220fff0f
Matt Thomson <red.cataclysm@gmail.com>
2016-10-10 09:30:48 -0400

    Connects to Local MongoDB

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,28 @@
 import Express from 'express';
+import mongoose from 'mongoose';
 
+// TODO: Pull database into a secret.
+const mongodbUri = 'mongodb://localhost/typing-teacher';
 const server = Express();
 const SERVER_PORT = 3000;
 
-server.use((req, res, next) => {
-  console.log(`${req.method}: ${req.originalUrl}`);
-  next();
-});
+mongoose.connect(mongodbUri, (err) => {
+  // TODO: Handle db conect error elegantly.
+  if (err) {
+    return err;
+  }
 
-server.get('*', (req, res) => {
-  res.end(`Hello world. Environment: ${process.env.NODE_ENV}`);
-});
+  const db = mongoose.connection;
+  server.use((req, res, next) => {
+    console.log(`${req.method}: ${req.originalUrl}`);
+    next();
+  });
 
-server.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}.`);
+  server.get('*', (req, res) => {
+    res.end(`Hello world. Environment: ${process.env.NODE_ENV}`);
+  });
+
+  server.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}.`);
+  });
 });
