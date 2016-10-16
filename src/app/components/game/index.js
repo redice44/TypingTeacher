fdbfa32930b827a9b32cdd54c835d0308ed4deb7
vesto001 <vicestopinan@yahoo.com>
2016-10-16 18:39:00 -0400

    Added Phrase and Time Trial buttons

--- src/app/components/game/index.js
+++ src/app/components/game/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+const style = {
+  margin: 12,
+};
 
 const Game = () => {
+	
   return (
     <Paper zDepth={1}>
+	  <RaisedButton
+        label='Phrase Mode'
+		style={style}
+		primary={true}
+      />	
+	  <RaisedButton
+        label='Time Trial Mode'
+		style={style}
+		primary={true}
+      />
       <p>This is a placeholder game.</p>
       <Link to='/dashboard'>Dashboard Link</Link>
     </Paper>
