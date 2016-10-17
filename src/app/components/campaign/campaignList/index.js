e3ca88373aee15886f65f6620f54c5434be4fe45
Matt Thomson <red.cataclysm@gmail.com>
2016-10-16 23:19:43 -0400

    Updates campaign list components

--- src/app/components/campaign/campaignList/index.js
+++ src/app/components/campaign/campaignList/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import { List, ListItem } from 'material-ui/List';
+import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import AddIcon from 'material-ui/svg-icons/content/add';
+import PlayIcon from 'material-ui/svg-icons/av/play-circle-outline';
+import MoreIcon from 'material-ui/svg-icons/navigation/more-horiz';
 
 const CampaignList = () => {
   // TODO: Actually get the campaigns from the user's account
@@ -9,12 +14,34 @@ const CampaignList = () => {
 
   for (let i = 0; i < campaignNum; i++) {
     campaigns.push(
-      <ListItem primaryText={`Campaign ${i}`} />
+      <ListItem
+        primaryText={`Campaign ${i}`}
+        // TODO: Make these icon buttons
+        leftIcon={<PlayIcon />}
+        rightIcon={<MoreIcon />}
+        onTouchTap={() => console.log(i)}
+      />
     );
   }
 
   return (
-    <div style={{width: '250px'}}>
+    <div style={{width: '500px'}}>
+      <Toolbar>
+        <ToolbarGroup
+        >
+          <ToolbarTitle
+            text='Campaign List'
+          />
+        </ToolbarGroup>
+        <ToolbarGroup
+          lastChild={true}
+          style={{alignItems: 'center'}}
+        >
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </ToolbarGroup>
+      </Toolbar>
       <List>
         {campaigns}
       </List>
