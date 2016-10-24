2e376134621b982b2c72dc5576ca07bd050c10d1
Matt Thomson <red.cataclysm@gmail.com>
2016-10-24 14:40:58 -0400

    Adds Icon link back to main page

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import request from 'superagent';
 
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
@@ -8,47 +7,23 @@ import ComputerIcon from 'material-ui/svg-icons/hardware/computer';
 import AccountIconButton from '../../containers/account.js';
 import TestAccountButton from '../../containers/testAccount.js';
 
-import accountUtil from '../../../util/account';
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-// TODO: Move to container
-function handleTouchTap() {
-  console.log('logging out');
-
-  request
-    .get(accountUtil.r.SIGN_OUT)
-    .end((err, res) => {
-      if (err) {
-        return console.log(err);
-      }
-
-      console.log(JSON.parse(res.text));
-    });
-}
-
-// TODO: Move to container
-function verify() {
-  console.log('verifying');
-  request
-    .get(accountUtil.r.VALIDATE)
-    .end((err, res) => {
-      if (err) {
-        return console.log(err);
-      }
-
-      console.log(JSON.parse(res.text));
-    });
-}
-
-const Header = () => {
-  return (
-    <AppBar
-      title = 'Typing Teacher'
-      onLeftIconButtonTouchTap={ verify }
-      onTitleTouchTap={ handleTouchTap }
-      iconElementLeft={ <IconButton><ComputerIcon /></IconButton> }
-      iconElementRight={ <div style={{display: 'flex'}}><TestAccountButton /><AccountIconButton /></div> }
-    />
-  );
+  render() {
+    return (
+      <AppBar
+        title = 'Typing Teacher'
+        onLeftIconButtonTouchTap={ () => this.context.router.push('/') }
+        iconElementLeft={ <IconButton><ComputerIcon /></IconButton> }
+        iconElementRight={ <div style={{display: 'flex'}}><TestAccountButton /><AccountIconButton /></div> }
+      />
+    );
+  }
 };
 
-export default Header;
+Header.contextTypes = {
+  router: React.PropTypes.object
+};
