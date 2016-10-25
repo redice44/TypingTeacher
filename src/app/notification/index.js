646465a01c06e185bf68903149f58d221502d969
Matt Thomson <red.cataclysm@gmail.com>
2016-10-25 10:54:16 -0400

    Fixes: Notifications

--- src/app/notification/index.js
+++ src/app/notification/index.js
@@ -1,36 +1,49 @@
-import React from 'react';
-
-// Material UI Components
-import Snackbar from 'material-ui/Snackbar';
-
+import { connect } from 'react-redux';
+import actionTypes from './actionTypes.js';
+import {
+  sendMessage,
+  sendError,
+  sendSuccess,
+  closeNotification
+} from './actions.js';
 import util from './constants';
+import Notification from './notification';
 
-const Notification = (props) => {
-  let styleClass = 'notification';
-
-  switch (props.notificationType) {
-    case util.c.MESSAGE:
-      styleClass = 'notification message';
-      break;
-    case util.c.ERROR:
-      styleClass = 'notification error';
-      break;
-    case util.c.SUCCESS:
-      styleClass = 'notification success';
-      break;
-    default:
-      // Do nothing.
-  }
-
-  return (
-    <Snackbar
-      open={props.open}
-      message={props.message}
-      className={styleClass}
-      autoHideDuration={4000}
-      onRequestClose={() => props.closeNotification()}
-    />
-  );
+// Convert store state to props to be passed to component
+const mapStateToProps = (state, ownProps) => {
+  // ownProps are props sent to the component
+  return {
+    // Set props to send
+    message: state.notification.message,
+    notificationType: state.notification.type,
+    open: state.notification.open
+  };
 };
 
-export default Notification;
+// Creates props that will dispatch actions
+const mapDispatchToProps = (dispatch, ownProps) => {
+  // ownProps are props sent to the component
+  return {
+    // Actions to send
+    sendMessage: (message) => {
+      dispatch(sendMessage(message));
+    },
+    sendError: (err) => {
+      dispatch(sendError(err));
+    },
+    sendSuccess: (message) => {
+      dispatch(sendSuccess(message));
+    },
+    closeNotification: () => {
+      dispatch(closeNotification());
+    },
+
+  };
+};
+
+const NotificationContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notification);
+
+export default NotificationContainer;
---
+++ src/app/notification/notification.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// Material UI Components
+import Snackbar from 'material-ui/Snackbar';
+
+import util from './constants';
+
+const Notification = (props) => {
+  let styleClass = 'notification';
+
+  switch (props.notificationType) {
+    case util.c.MESSAGE:
+      styleClass = 'notification message';
+      break;
+    case util.c.ERROR:
+      styleClass = 'notification error';
+      break;
+    case util.c.SUCCESS:
+      styleClass = 'notification success';
+      break;
+    default:
+      // Do nothing.
+  }
+
+  return (
+    <Snackbar
+      open={props.open}
+      message={props.message}
+      className={styleClass}
+      autoHideDuration={4000}
+      onRequestClose={() => props.closeNotification()}
+    />
+  );
+};
+
+export default Notification;
